a0cbe4cba5aebf35a4edf40e87b7ea1ddeca762f
Derek Gauger <gaugerderek@gmail.com>
2022-12-08 09:36:26 -0600

    Changed scraped website
    nationaldayscalendar.com was unreliable

--- national_days_scraper.py
+++ national_days_scraper.py
@@ -1,36 +1,60 @@
+from locale import currency
 from bs4 import BeautifulSoup
 import requests
 import re
 from datetime import datetime
 import json
 
-today = datetime.now()
 
-current_month = today.strftime('%B')
-current_day = today.strftime("%d")
+TODAY_URL = "https://nationaltoday.com/what-is-today/"
+PATH_TO_JSON = "/home/ubunut/truth_discord_bot/national_days.json"
+# PATH_TO_JSON = "./national_days.json"
 
-date_dict = {}
-date_dict[current_month] = {}
-date_dict[current_month][str(int(current_day))] = []
 
-def getHTMLdocument(url):
+def get_HTML_document(url):
     response = requests.get(url)
     return response.text
 
-def HTMLtoAscii(text):
+
+def HTML_to_ascii(text):
     return str(text.encode('ascii', 'ignore').decode('utf-8'))
 
-url = "https://nationaldaycalendar.com/what-day-is-it/"
 
-html_document = getHTMLdocument(url)
+def remove_duplicates(list):
+    filtered_list =  []
+    for element in list:
+        if element not in filtered_list:
+            filtered_list.append(element)
 
-soup = BeautifulSoup(html_document, 'html.parser')
+    return  filtered_list
 
-for span in soup.find_all('span', attrs={'class': re.compile("^evcal_desc2 evcal_event_title")}):
-    matches = re.search(r">(.*)</", str(span))
-    if matches:
-        date_dict[current_month][str(int(current_day))].append(HTMLtoAscii(matches.groups()[0]))
 
-with open('/home/ubuntu/truth_discord_bot/national_days.json', 'w') as f:
-    f.write(json.dumps(date_dict, indent=4))
+def create_national_day_dictionary():
 
+    today = datetime.now()
+
+    current_month = today.strftime('%B')
+    current_day = today.strftime("%d")
+    current_day = str(int(current_day))
+
+    date_dict = {}
+    date_dict[current_month] = {}
+    date_dict[current_month][current_day] = []
+
+    html_document = get_HTML_document(TODAY_URL)
+    soup = BeautifulSoup(html_document, 'html.parser')
+
+    for i in remove_duplicates(soup.find_all('h3', attrs={'class': re.compile("^holiday-title")})):
+        title = HTML_to_ascii(i.text)
+        if not "birthday" in title.lower():
+            date_dict[current_month][current_day].append(title)
+
+    return date_dict
+
+def main():
+
+    date_dict_today = create_national_day_dictionary()
+
+    open(PATH_TO_JSON, 'w').write(json.dumps(date_dict_today, indent=4))
+
+main()
