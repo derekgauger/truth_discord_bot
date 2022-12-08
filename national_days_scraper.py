1a60646039063e519f0dffb9969cc4556ad1dac3
Derek Gauger <gaugerderek@gmail.com>
2022-12-08 10:13:28 -0600

    Getting short blurb
    Adding a function to get and write a short blurb about the day to a .txt file

--- national_days_scraper.py
+++ national_days_scraper.py
@@ -6,9 +6,17 @@ from datetime import datetime
 import json
 
 
-TODAY_URL = "https://nationaltoday.com/what-is-today/"
-PATH_TO_JSON = "/home/ubuntu/truth_discord_bot/national_days.json"
-# PATH_TO_JSON = "./national_days.json"
+today = datetime.now()
+
+current_month = today.strftime('%B')
+current_day = str(int(today.strftime("%d")))
+
+# TODAY_URL = "https://nationaltoday.com/what-is-today/"
+TODAY_URL = "https://nationaltoday.com/{}-{}-holidays/".format(current_month.lower(), current_day)
+# PATH_TO_JSON = "/home/ubunut/truth_discord_bot/national_days.json"
+# PATH_TO_TXT = "/home/ubunut/truth_discord_bot/national_day_blurb.txt"
+PATH_TO_JSON = "./national_days.json"
+PATH_TO_TXT = "./national_day_blurb.txt"
 
 
 def get_HTML_document(url):
@@ -31,12 +39,6 @@ def remove_duplicates(list):
 
 def create_national_day_dictionary():
 
-    today = datetime.now()
-
-    current_month = today.strftime('%B')
-    current_day = today.strftime("%d")
-    current_day = str(int(current_day))
-
     date_dict = {}
     date_dict[current_month] = {}
     date_dict[current_month][current_day] = []
@@ -51,9 +53,28 @@ def create_national_day_dictionary():
 
     return date_dict
 
-def main():
 
+def get_national_day_blurb():
+    retVal = None
+    html_document = get_HTML_document(TODAY_URL)
+    soup = BeautifulSoup(html_document, 'html.parser')
+    for i in soup.find_all('p'):
+        paragraph = HTML_to_ascii(i.text)
+        if paragraph.startswith("{} {}".format(current_month, current_day)):
+            retVal = paragraph
+            break
+    
+    return retVal
+    
+
+def main():
     date_dict_today = create_national_day_dictionary()
+    day_blurb = get_national_day_blurb()
+
+    if day_blurb:
+        open(PATH_TO_TXT, 'w').write(day_blurb)
+    else:
+        open(PATH_TO_TXT, 'w').write("I have no fun facts for today. Maybe I will have some tomorrow. Who knows :)")
 
     open(PATH_TO_JSON, 'w').write(json.dumps(date_dict_today, indent=4))
 
