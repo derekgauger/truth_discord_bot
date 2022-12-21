18f72dab48a6c19ac8dbe486505db15bd5f526a8
Dominik K <dominik@koch-bautechnik.de>
2022-12-21 12:19:35 +0100

    Add vote button

--- commands/display/info.js
+++ commands/display/info.js
@@ -1,4 +1,4 @@
-const { SlashCommandBuilder } = require('discord.js')
+const { SlashCommandBuilder, ActionRowBuilder, ButtonBuilder } = require('discord.js')
 require('../../functions/discord_messages/createInfo')
 
 module.exports = {
@@ -6,7 +6,7 @@ module.exports = {
         .setName('info')
         .setDescription("Sends a link to bot information")
         .setDescriptionLocalizations({
-            de: 'Sendet einen Link zu Bot-Informationen',
+            de: 'Sendet einen link zu Bot Informationen',
         }),
 
     async execute(interaction, client) {
@@ -14,8 +14,17 @@ module.exports = {
         let embed = await client.createInfo()
         console.log(`'${interaction.user.username}' used /info in '${interaction.guild.name}'`)
 
+        const button = new ActionRowBuilder().addComponents(
+            new ButtonBuilder()
+              .setLabel("Vote for Truth")
+              .setStyle(5)
+              .setEmoji('🔮')
+              .setURL('https://top.gg/bot/929121472138604614/vote'),
+          );
+
         await interaction.reply({
-            embeds: [embed]
+            embeds: [embed],
+            components: [button]
         }).catch(err => console.log(err))
     }
 }
