9c146e846ea70c08973ce1fb4a2522589c323860
Dominik K <dominik@koch-bautechnik.de>
2022-12-23 00:38:13 +0100

    Fix interactionCreate.js line 3

--- events/client/interactionCreate.js
+++ events/client/interactionCreate.js
@@ -1,15 +1,30 @@
 module.exports = (client, interaction) => {
-    if (interaction.isChatInputCommand())
-    const command = client.commands.get(interaction.commandName);
-    if (!command) return;
-    try {
-      command.execute(interaction, client);
-    } catch (err) {
-      if (err) console.error(err);
-      interaction.reply({
-        content: "An error occurred while executing that command.",
-        ephemeral: true,
-      });
-    }
+  if (!interaction.guild) {
+    if (interaction.isChatInputCommand()) {
+      const command = client.commands.get(interaction.commandName);
+      if (!command) return;
+      try {
+        command.execute(interaction, client);
+      } catch (err) {
+        if (err) console.error(err);
+        interaction.reply({
+          content: "An error occurred while trying to execute that command.",
+          ephemeral: true,
+        });
+      }
+    };
+  } else {
+    if (interaction.isChatInputCommand()) {
+      const command = client.commands.get(interaction.commandName);
+      if (!command) return;
+      try {
+        command.execute(interaction, client);
+      } catch (err) {
+        if (err) console.error(err);
+        interaction.reply({
+          content: "An error occurred while trying to execute that command.",
+          ephemeral: true,
+        });
+      }
   };
   
