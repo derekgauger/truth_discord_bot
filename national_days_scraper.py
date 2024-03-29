0c5f71baf6675b4fd06b41dbc56e716905a0865c
Derek Gauger <gaugerderek@gmail.com>
2024-03-29 08:59:06 -0500

    Update national_days_scraper.py
    Fixing blurb duplicating issue.

--- national_days_scraper.py
+++ national_days_scraper.py
@@ -22,7 +22,7 @@ MAX_FACTS = MAX_DATES = MAX_BIRTHDAYS = MAX_DEATHS = 5
 MAX_DAYS = 15
 
 DATE_TODAY = date.today().strftime('%B-%d').lower()
-FACTS_URL = "https://www.thefactsite.com/day/today/"
+FACTS_URL = f"https://www.thefactsite.com/day/{DATE_TODAY}/"
 # DAYS_URL = "https://nationaltoday.com/" + DATE_TODAY + '-holidays/'
 DAYS_URL = "https://nationaltoday.com/today/"
 
